3d12855b4bf028ac45b8838a0bc28e4340569e53
chrisyates <chris@christianyates.com>
2011-07-02 11:46:15 -0400

    Added currently working files for iOS and changes to phonegap files

---
+++ DrupalGeoPicture/Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  DrupalGeoPicture
+//
+//  Created by Christian Yates on 7/2/11.
+//  Copyright Christian Yates Interactive 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit DrupalGeoPicture.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
---
+++ DrupalGeoPicture/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DrupalGeoPicture
+//
+//  Created by Christian Yates on 7/2/11.
+//  Copyright Christian Yates Interactive 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
